07ece8a43bc09ce977e9a51423a91ee1882eb07e
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2023-10-13 12:13:46 +0900

    Update: 単語IDを指定して表示できるよう変更

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+from typing import Optional
 
 
 data_file_path = Path(__file__).parent / "dictionary-data.txt"
 
 
-def print_dictionary(path: Path) -> None:
-    _data_file_contents = data_file_path.read_text()
-    dict_data = _data_file_contents.strip()  # 最終行が空行だった場合、その行を削除する
+def print_dictionary(path: Path, index: Optional[int] = None) -> None:
+    _data_file_contents = path.read_text()
+    _data_file_contents = _data_file_contents.strip()  # 最終行が空行だった場合、その行を削除する
+    dict_data = _data_file_contents.splitlines()
 
-    for i, line in enumerate(dict_data.splitlines()):
-        print(f"{i + 1}: {line}")
+    if isinstance(index, int) and (0 < index <= len(dict_data)):
+        print(f"{index}: {dict_data[index - 1]}")
+    elif index is not None:
+        # 与えられた index が単語IDの範囲外だった場合
+        print(f"単語のIDは1から{len(dict_data)}までです。")
+    else:
+        for i, line in enumerate(dict_data):
+            print(f"{i + 1}: {line}")
 
 
 if __name__ == "__main__":
@@ -24,7 +32,10 @@ if __name__ == "__main__":
         default=str(data_file_path),
         nargs="?",
     )
+    parser.add_argument(
+        "--index", "-i", type=int, help="表示する単語のID", required=False
+    )
 
     args = parser.parse_args()
 
-    print_dictionary(Path(args.file))
+    print_dictionary(Path(args.file), args.index)
