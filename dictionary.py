dabf34f250017d17bd44aa8d49a9aecc2de97914
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2023-10-13 11:23:31 +0900

    Update: 仕様3を実装
    辞書データをテキストファイルに分割

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
-print("上手")
+from pathlib import Path
+
+
+data_file_path = Path(__file__).parent / "dictionary-data.txt"
+
+_data_file_contents = data_file_path.read_text()
+dict_data = _data_file_contents.strip()  # 最終行が空行だった場合、その行を削除する
+
+print(dict_data)
