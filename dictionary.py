8c9a448dbe44afdcb0575386e7a7ecf229b140a6
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2023-10-13 11:46:46 +0900

    Update: ファイルから読み込んだ辞書データを1つひとつ分割して扱うよう変更
    挙動に変化はないが、ファイルの中身をそのまま使うのは辞書データの扱いとして適切ではないため

--- dictionary.py
+++ dictionary.py
@@ -9,7 +9,8 @@ def print_dictionary(path: Path) -> None:
     _data_file_contents = data_file_path.read_text()
     dict_data = _data_file_contents.strip()  # 最終行が空行だった場合、その行を削除する
 
-    print(dict_data)
+    for line in dict_data.splitlines():
+        print(line)
 
 
 if __name__ == "__main__":
